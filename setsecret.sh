bba995aa345c9400310372807b2fdc0c31412e1a
John Duimovich <jduimovich@gmail.com>
2021-04-04 21:34:31 -0400

    quay.io

--- setsecret.sh
+++ setsecret.sh
@@ -4,11 +4,10 @@ SERVER=$(oc whoami --show-server )
 
 gh secret set OPENSHIFT_SERVER -b $SERVER
 gh secret set OPENSHIFT_TOKEN -b $TOKEN
-if [ -z $MY_DOCKER_PW ]
+if [ -z $MY_QUAY_PW ]
 then
-echo "\n" $0: No MY_DOCKER_PW set, please set MY_DOCKER_PW to your registry password.
-echo "\n" $0: This registry must match the registry (docker.io or quay.io you have set in the OpenShift Workflow)
+echo  $0: No MY_QUAY_PW set, please set MY_QUAY_PW to your registry password.
 exit
 else 
-gh secret set REGISTRY_PASSWORD -b $MY_DOCKER_PW
+gh secret set REGISTRY_PASSWORD -b $MY_QUAY_PW
 fi
